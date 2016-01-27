864fd1b3cc73d5316a9019720cc5824c53960f9a
trineon89 <trineon89@gmail.com>
2016-01-27 13:46:47 +0100

    Base Change, first class

--- UVA.lua
+++ UVA.lua
@@ -1,6 +1,20 @@
-﻿local EventFrame = CreateFrame("Startup_Login")
+﻿local GlobalAddonName, UVA = ...
 
-EventFrame:RegisterEvent("PLAYER_LOGIN")
-EventFrame:SetScript("OnEvent", function(self,event,...) 
-     	ChatFrame1:AddMessage('UVA Loaded ".. UnitName("Player"))
-end)
+UVA.V = 1
+UVA.Rt = "R"
+
+--############### Client Version ###############--
+
+local version, buildVersion, buildDate, uiVersion = GetBuildInfo()
+	UVA.clientUIinterface = uiVersion
+	local expansion,majorPatch,minorPatch = (version or "1.0.0"):match("^(%d+)%.(%d+)%.(%d+)")
+	UVA.clientVersion = (expansion or 0) * 10000 + (majorPatch or 0) * 100 + (minorPatch or 0)
+end
+
+--############### Addon Main Window Frame ###############--
+
+local reloadTimer = 0.1
+UVA.frame = CreateFrame("Frame")
+
+
+UVA.frame:RegisterEvent("ADDON_LOADED") 
