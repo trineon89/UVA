d7d50420e15a395100871bf72ddea894208c0224
trineon89 <trineon89@gmail.com>
2016-01-27 13:28:19 +0100

    Base File Structure Created

---
+++ UVA.lua
@@ -0,0 +1,6 @@
+﻿local EventFrame = CreateFrame("Startup_Login")
+
+EventFrame:RegisterEvent("PLAYER_LOGIN")
+EventFrame:SetScript("OnEvent", function(self,event,...) 
+     	ChatFrame1:AddMessage('UVA Loaded ".. UnitName("Player"))
+end)
